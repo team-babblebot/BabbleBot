31a68aee43225f97aa416d622bc5e89391f72178
Vi <vcarr@pm.me>
2019-06-28 18:33:05 -0500

    Added capitalization tweaks to boomer mode

--- boomer.py
+++ boomer.py
@@ -6,3 +6,27 @@ def add_elipses(sentence):
         if random.randint(1,10) <= 7:
             words[i] += "..."
     return " ".join(words)
+
+def boomer_caps(sentence):
+    seed = random.randint(1, 10)
+    sent_array = sentence.split()
+    if seed in (1, 2, 3):
+        return sentence
+    elif seed in (4, 5):
+        temp_sent = []
+        for x in sent_array:
+            if random.random() < 0.25:
+                x = x.upper()
+            temp_sent.append(x)
+        return " ".join(temp_sent)
+    elif seed in (6, 7):
+        temp_sent = []
+        for x in sent_array:
+            if random.random() < 0.5:
+                x = x.upper()
+            temp_sent.append(x)
+        return " ".join(temp_sent)
+    elif seed in (8, 9):
+        return sentence.title()
+    elif seed == 10:
+        return sentence.upper()
