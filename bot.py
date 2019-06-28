2c06e7021e48bdcf014d446eb512ff54c9baf803
JohnnySn0w <michael.mahan43@gmail.com>
2019-06-28 17:39:31 -0500

    pings are now toggleable

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import markov
 import discord
 import re
 
-from config import DEBUG, TOKEN, PREFIX as BOT_PREFIX, BotPingCredits
+from config import DEBUG, TOKEN, PREFIX as BOT_PREFIX, BOT_PING_CREDITS, ALLOW_SENTENCE_MENTIONS
 
 MESSAGES_DIRECTORY = "messages/"
 
@@ -43,25 +43,31 @@ async def babble(ctx, arg = [], limit: int=10000):
                 f.write(msg + '\n')
         if DEBUG: print(channel.id)
         if DEBUG: print("hey i'm makin a file here")
-    
-    r = re.compile('<@!?(\d+)>')
+
     with open(outfile, 'r') as words:
         corpus = ''
         words = words.readlines()
         for x in words:
-            if r.match(x) is not None:
-                pinged_uid = int(re.sub(r'[^\d]', '', x))
-                pinged = server.get_member(int(pinged_uid))
-                if pinged.nick == None:
-                    x = "[" + pinged.name + "]"
-                else:
-                    x = "[" + pinged.nick + "]"
             corpus += x
         sentence = markov.gen_sentence(markov.create_markov_model(corpus))
+
+        if not ALLOW_SENTENCE_MENTIONS:
+            match = re.search(r'<@!?(\d+)>', sentence)
+            print(f'SENTENCE: {sentence}')
+            print(f'match: {match}')
+            if match:
+                for user_id in match.groups():
+                    user_id = int(user_id)
+                    pinged_user = server.get_member(user_id)
+                    if pinged_user.nick == None:
+                        sentence = re.sub(r'<@!?\d+>', pinged_user.name, sentence)
+                    else:
+                        sentence = re.sub(r'<@!?\d+>', pinged_user.nick, sentence)
+
         if original:
             await channel.send(sentence)
         else:
-            if BotPingCredits:
+            if BOT_PING_CREDITS:
                 await channel.send("\"" + sentence + "\" -<@!" + str(ctx.message.mentions[0].id) + ">")
             else:
                 if ctx.message.mentions[0].nick == None:
@@ -69,26 +75,31 @@ async def babble(ctx, arg = [], limit: int=10000):
                 else:
                     await channel.send("\"" + sentence + "\" -" + str(ctx.message.mentions[0].nick))
 
-@client.command(pass_context=True, aliases=['archive'])
-async def relog(ctx, *limit:int):
+@client.command(pass_context=True, aliases=['r'])
+async def relog(ctx, arg = [], *limit:int):
     list_messages = []
+    if arg == []:
+        called_user = ctx.message.author
+    else:
+        called_user = ctx.message.mentions[0]
     channel = ctx.message.channel
-    calleduser = ctx.message.author
     if DEBUG:
-        print(calleduser.id)
+        print(called_user.id)
     async for message in channel.history(limit=10000):
-        if message.author == calleduser:
+        if message.author == called_user:
             list_messages.append(message.content)
     if DEBUG:
         for i in list_messages:
             print(i)
-    outfile = f'{MESSAGES_DIRECTORY}{channel.id}/{calleduser.id}.out'
+    outfile = f'{MESSAGES_DIRECTORY}{channel.id}/{called_user.id}.out'
     os.remove(outfile)
     os.makedirs(os.path.dirname(outfile), exist_ok=True)
     with open(outfile, 'w') as f:
         for i in list_messages:
             f.write(i + '\n')
     if DEBUG: print(channel.id)
+    await channel.send(f"Relog complete for {called_user.name}!")
+
 @client.command(pass_context=True, aliases=['h'])
 async def help(ctx, limit: int=10000):
     await ctx.message.channel.send(
